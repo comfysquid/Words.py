ce73594a44316b7a117d138047950d198882aab4
comfysquid <105414146+comfysquid@users.noreply.github.com>
2022-05-12 20:26:13 +1000

    Update words.py

--- words.py
+++ words.py
@@ -1,8 +1,10 @@
+"""Retrieve and print words from a URL"""
 import sys
 from urllib import request
 
 
 def fetch_words(url):
+    """Fetch a list of words from a URL."""
     story=request.urlopen(url)
     story_words=[]
     for line in story:
@@ -14,14 +16,16 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Print items one per line."""
     for item in items:
         print(item)
 
 
 def main(url):
+    """Print each word from a text document from at a URL"""
     words=fetch_words(url)
     print_items(words)
 
 
 if __name__=='__main__':
-    main(sys.argv[1])
+    main(sys.argv[1]) # The 0th arg is the module filename.
