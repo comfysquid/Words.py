79795d0059a4667f33115ddd14b617e02ad56edb
comfysquid <105414146+comfysquid@users.noreply.github.com>
2022-05-12 18:47:49 +1000

    Update words.py

--- words.py
+++ words.py
@@ -1,10 +1,13 @@
+from urllib import request
+
+
 def fetch_words():
-    story=urlopen('http://sixty-north.com/c/t.txt')
+    story=request.urlopen('http://sixty-north.com/c/t.txt')
     story_words=[]
     for line in story:
         line_words=line.decode('utf8').split()
-    for word in line_words:
-        story_words.append(word)
+        for word in line_words:
+            story_words.append(word)
     story.close()
 
 
